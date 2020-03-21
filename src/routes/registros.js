7bb20c9610937cf95a3d8cc65278298edbe564d5
AndersonRujeles <60858558+AndersonRujeles@users.noreply.github.com>
2020-03-21 20:11:50 +0100

    cambios

--- src/routes/registros.js
+++ src/routes/registros.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router= express.Router();
+const pool=require('../database');
 
 router.get('/registra',(req,res)=>{
-   // res.send('oh yeah')
+  
    res.render('registros/registra');
+});
+
+router.post('/registra',async(req,res)=>{
+    
+   const {nombre,usuario,correo,contrasena}=req.body;
+   const registrar={nombre,usuario,correo,contrasena};
+   await pool.query('insert into registro set ?',[registrar]);
+   res.redirect('/registros/registra',{envio});
+   console.log("registro exitoso");
 });
 module.exports=router;
