c86d11926ead1d403f63036d573b524ce374b8c6
AndersonRujeles <60858558+AndersonRujeles@users.noreply.github.com>
2020-05-16 12:05:41 -0500

    cambios

--- src/routes/carrito.js
+++ src/routes/carrito.js
@@ -7,10 +7,14 @@ const {estalogueado}=require('../lib/valida');
 router.get('/carrito',estalogueado,async(req,res)=>{
     const muestra=await pool.query('select * from carrito');
     const cant=await pool.query('select sum(cantidad) from carrito');
-    console.log(cant);
     res.render('links/carrito',{muestra,cant});
  });
 
+ router.post('/delete-carrito/:idcarrito',estalogueado,async(req,res)=>{
+    const {idcarrito}=req.params;
+    await pool.query('delete from carrito where idcarrito = ?',[idcarrito]);
+    res.redirect('../carrito');
+ });
 
 
 module.exports=router;
