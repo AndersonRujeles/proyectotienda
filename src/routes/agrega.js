0b617b61618c3f9d641bca00d2f0e4b9222cc1bf
AndersonRujeles <60858558+AndersonRujeles@users.noreply.github.com>
2020-05-23 14:19:30 -0500

    cambios

--- src/routes/agrega.js
+++ src/routes/agrega.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router= express.Router();
 const pool=require('../database');
 
+
 router.get('/agrega',(req,res)=>{
     res.render('administrador/addproduc');
    });
--- src/routes/carrito.js
+++ src/routes/carrito.js
@@ -9,9 +9,9 @@ const formatterPeso = new Intl.NumberFormat('es-CO', {
  })
 
 router.get('/carrito',estalogueado,async(req,res)=>{
-    const muestra=await pool.query('select idcarrito,nombre,cantidad,precio,precio*cantidad as totales from carrito where idcliente = ?',[req.user.idcliente]);
-    const consultaCantidad=await pool.query('select sum(cantidad) as cantProducto,sum(precio) as precioProducto from carrito where idcliente = ?',[req.user.idcliente]);
-    const consultaTotal=await pool.query('select sum(precio*cantidad) as total from carrito where idcliente = ?',[req.user.idcliente]);
+    const muestra=await pool.query('select idcarrito,nombre,cantidad,precio,precio*cantidad as totales from carrito where idcliente = ? and idestado=1',[req.user.idcliente]);
+    const consultaCantidad=await pool.query('select sum(cantidad) as cantProducto,sum(precio) as precioProducto from carrito where idcliente = ? and idestado=1',[req.user.idcliente]);
+    const consultaTotal=await pool.query('select sum(precio*cantidad) as total from carrito where idcliente = ?  and idestado=1',[req.user.idcliente]);
     const cantidad=consultaCantidad[0].cantProducto;
     const precioUnidad=formatterPeso.format(consultaCantidad[0].precioProducto);
     const totalCompra=formatterPeso.format(consultaTotal[0].total);
@@ -24,6 +24,12 @@ router.get('/carrito',estalogueado,async(req,res)=>{
     await pool.query('delete from carrito where idcarrito = ?',[idcarrito]);
     res.redirect('../carrito');
  });
+
+ router.post('/comprar',async(req,res)=>{
+   const productoact=await pool.query('UPDATE carrito set idestado=2 where idcliente= ?',[req.user.idcliente]);
+   req.flash('success','Compra realizada exitosamente');
+   res.redirect('../carrito');
+});
 
 
 module.exports=router;
